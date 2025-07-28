c3359f295d32940c198bc1a8db0c14271cfec0e5
bruno.lr <bruno.lr@sidi.org.br>
2025-07-27 20:25:37 -0400

    fix: Ajusta layout para respeitar as barras do sistema

--- app/src/main/java/com/picpay/desafio/android/presentation/ui/userlist/UserListScreen.kt
+++ app/src/main/java/com/picpay/desafio/android/presentation/ui/userlist/UserListScreen.kt
@@ -1,54 +1,60 @@
 package com.picpay.desafio.android.presentation.ui.userlist
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun UserListRoute(
     viewModel: UserListViewModel = hiltViewModel()
 ) {
-    // Coleta o estado do ViewModel
     val state by viewModel.state.collectAsState()
-    // Passa o estado para a nossa tela "burra"
     UserListScreen(state = state)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UserListScreen(
     state: UserListState,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(top = 16.dp)
-    ) {
-        Text(
-            text = "Contatos",
-            style = MaterialTheme.typography.headlineMedium,
-            modifier = Modifier.padding(horizontal = 16.dp)
-        )
+
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Contatos", style = MaterialTheme.typography.headlineMedium) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Black
+                )
+            )
+        }
+    ) { innerPadding ->
 
         Box(
             modifier = Modifier
+                .background(color = Color.Black)
                 .fillMaxSize()
-                .padding(top = 16.dp),
+                .padding(innerPadding),
             contentAlignment = Alignment.Center
         ) {
             if (state.users.isNotEmpty()) {
