c73d7190137743ca95dbd24e1939ab2b70e8215c
bruno.lr <bruno.lr@sidi.org.br>
2025-07-28 08:32:03 -0400

    refactor(domain): Injeta Logger no GetUsersUseCase para depuração
    - Injeta a interface Logger no GetUsersUseCase para permitir o registro de eventos de forma desacoplada.
    - Atualiza o GetUsersUseCaseTest para fornecer uma implementação falsa (TestLogger) da nova dependência, mantendo o teste unitário isolado e rápido.

--- app/src/main/java/com/picpay/desafio/android/domain/usecase/GetUsersUseCase.kt
+++ app/src/main/java/com/picpay/desafio/android/domain/usecase/GetUsersUseCase.kt
@@ -1,13 +1,22 @@
 package com.picpay.desafio.android.domain.usecase
 
+import com.picpay.desafio.android.domain.common.Logger
 import com.picpay.desafio.android.domain.common.Resource
 import com.picpay.desafio.android.domain.model.User
 import com.picpay.desafio.android.domain.repository.UserRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 class GetUsersUseCase @Inject constructor(
-    private val repository: UserRepository
+    private val repository: UserRepository,
+    private val logger: Logger
 ) {
-    operator fun invoke(): Flow<Resource<List<User>>> = repository.getUsers()
+    operator fun invoke(): Flow<Resource<List<User>>> = repository.getUsers().onStart {
+        logger.d(TAG, "Buscando lista de usuários...")
+    }
+
+    companion object {
+        private const val TAG = "GetUsersUseCase"
+    }
 }
--- app/src/test/java/com/picpay/desafio/android/domain/use/GetUsersUseCaseTest.kt
+++ app/src/test/java/com/picpay/desafio/android/domain/use/GetUsersUseCaseTest.kt
@@ -1,7 +1,9 @@
 package com.picpay.desafio.android.domain.use
 
+import com.picpay.desafio.android.domain.common.Logger
 import com.picpay.desafio.android.domain.repository.UserRepository
 import com.picpay.desafio.android.domain.usecase.GetUsersUseCase
+import com.picpay.desafio.android.presentation.userlist.TestLogger
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
@@ -11,8 +13,9 @@ import org.junit.Test
 class GetUsersUseCaseTest {
 
     private val repository: UserRepository = mockk(relaxed = true)
+    private val fakeLogger: Logger = TestLogger()
 
-    private val getUsersUseCase = GetUsersUseCase(repository)
+    private val getUsersUseCase = GetUsersUseCase(repository, fakeLogger)
 
     @Test
     fun `when use case is invoked, it should call getUsers from repository`() = runTest {
