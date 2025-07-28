286c292097d318a411492add2afd1d9ab01c5633
bruno.lr <bruno.lr@sidi.org.br>
2025-07-27 20:38:05 -0400

    refactor: Melhora o tratamento do estado de erro na UI

--- app/src/main/java/com/picpay/desafio/android/presentation/ui/userlist/UserListState.kt
+++ app/src/main/java/com/picpay/desafio/android/presentation/ui/userlist/UserListState.kt
@@ -5,5 +5,5 @@ import com.picpay.desafio.android.domain.model.User
 data class UserListState(
     val isLoading: Boolean = false,
     val users: List<User> = emptyList(),
-    val error: String? = ""
+    val error: String? = null
 )
